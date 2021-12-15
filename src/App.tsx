d0034da35b2b425561f9aae4f512ddb7bf136ec8
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-12-15 12:57:29 -0600

    window resizing works.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [currentWindow, setCurrentWindow] = useState(null as any);
+  useEffect(() => {
+    const setWindowState = async () => {
+      const thisWindow = await chrome.windows.getCurrent()
+      setCurrentWindow(thisWindow)
+    };
+    setWindowState();
+  }, []);
+  const popupWidth = (currentWindow !== null) ? currentWindow.width * 0.5 : 300;
   return (
-    <div>
-          Hello World
+    <div style={{ width: popupWidth }}>
+      Hello World {popupWidth}
+      !!!
     </div>
   );
 }
