eeeff753dc39d084400637c33f8d8cef60cf81ea
Furkan Toprak <furkancemaltoprak@gmail.com>
2021-12-17 18:55:14 -0600

    TODO: modify pagination.

--- src/components/HistoryTable.tsx
+++ src/components/HistoryTable.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Table, TablePagination, TableSortLabel, TableHead, TableBody, TableCell, TableContainer, TableRow } from "@mui/material"
+import { TextField, Table, TablePagination, TableSortLabel, TableHead, TableBody, TableCell, TableContainer, TableRow } from "@mui/material"
 import { purple } from "../styles/Theme";
 
 interface TableItem {
@@ -16,16 +16,40 @@ export default function HistoryTable() {
     const [rowsPerPage, setRowsPerPage] = useState(5);
     const [page, setPage] = useState(0);
     const [order, setOrder] = useState('desc');
+    const [init, setInit] = useState(false);
     const [history, setHistory] = useState([] as TableItem[]);
-    chrome.history.search({ text: '', maxResults: 10 }, function (data) {
-        const fetchedHistory = data.map((entry: chrome.history.HistoryItem) => {
-            const taggedEntry: TableItem = entry;
-            taggedEntry.tag = "woah"
-            return taggedEntry
-        })
-        setHistory(fetchedHistory)
-    });
+    const searchHistory = (searchText: string) => {
+        chrome.history.search({ text: searchText }, function (data) {
+            const fetchedHistory = data.map((entry: chrome.history.HistoryItem) => {
+                const taggedEntry: TableItem = entry;
+                taggedEntry.tag = "woah"
+                return taggedEntry
+            })
+            setInit(true)
+            setHistory(fetchedHistory)
+        });
+    }
+    if (!init) {
+        searchHistory('');
+    }
     return <div style={{ maxHeight: 550 }}>
+        <div style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+            <TextField id="outlined-basic" label="Search History" defaultValue={""} onChange={(event) => searchHistory(event.target.value)} />
+        </div>
+        <TablePagination
+            rowsPerPageOptions={[5, 10, 25, 50]}
+            component="div"
+            count={history.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={(_event, newPage: number) => {
+                setPage(newPage)
+            }}
+            onRowsPerPageChange={(event: any) => {
+                setRowsPerPage(parseInt(event.target.value, 10));
+                setPage(0);
+            }}
+        />
         <TableContainer>
             <Table stickyHeader={true}>
                 <TableHead>
@@ -52,20 +76,6 @@ export default function HistoryTable() {
                 </TableBody>
             </Table>
         </TableContainer>
-        <TablePagination
-            rowsPerPageOptions={[5, 10, 25, 50]}
-            component="div"
-            count={history.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={(_event, newPage: number) => {
-                setPage(newPage)
-            }}
-            onRowsPerPageChange={(event: any) => {
-                setRowsPerPage(parseInt(event.target.value, 10));
-                setPage(0);
-            }}
-        />
     </div>
 }
 
